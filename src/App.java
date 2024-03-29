f78e3c04210b9f2bf24c15dbadf7146d2e168883
alstlr0307 <alstlr0307@naver.com>
2024-03-29 10:06:27 +0900

    3월 22일 강의내용

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var foo = 200;
+        var name = "woogie";
+
+        System.out.println(foo + name);
     }
 }
